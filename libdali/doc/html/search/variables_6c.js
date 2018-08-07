3c4d1a349c695315ad0bcd510bb8464ba2b5bf8b
Chad Trabant <chad.trabant@iris.edu>
2018-08-07 15:21:22 -0700

    add some missing files from libdali

---
+++ libdali/doc/html/search/variables_6c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['link',['link',['../struct_d_l_c_p__s.html#a72cd7b06d4208e54dfb6c7a0cc533897',1,'DLCP_s']]],
+  ['log',['log',['../struct_d_l_c_p__s.html#a27d0e4079f0f9a06f923a499b9eeac5e',1,'DLCP_s']]],
+  ['log_5fprint',['log_print',['../struct_d_l_log__s.html#a981ee326561e36c7df377418af278138',1,'DLLog_s']]],
+  ['logprefix',['logprefix',['../struct_d_l_log__s.html#a021b57accc0c31f0d8d2e043c89add3f',1,'DLLog_s']]]
+];
